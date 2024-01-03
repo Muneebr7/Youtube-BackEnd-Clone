be388ca62a3f16ad3c08f9d997acd2a366bacb5d
Muneeb ur Rehman <muneebofficial@gmail.com>
2024-01-03 20:50:32 +0500

    basic setup

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import {asyncHandler} from '../utils/asyncHandler.js'
 const regiserUser = asyncHandler( async (req,res)=>{
 
     const {fullName , email, username , password} =  req.body
-    console.log("email :" , email)
+   
 })
 
 

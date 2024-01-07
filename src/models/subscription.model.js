bfb93a13f1bfd95e15dfa39fcf7aa9a867529b88
Muneeb ur Rehman <muneebofficial@gmail.com>
2024-01-08 02:17:23 +0500

    subscription model added

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+
+        subscriber : {
+            type : Schema.Types.ObjectId, // subscribing
+            ref: 'User'
+        },
+        channel : {
+            type : Schema.Types.ObjectId, //subscriber
+            ref: 'User'
+        }
+
+
+}, {timestamps : true})
+
+
+
+export const Subscription = mongoose.model("Subscription" , subscriptionSchema)

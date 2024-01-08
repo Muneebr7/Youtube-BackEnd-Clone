05f2dbb8199704e482bade8d7dae5ceffe4605d2
Muneeb ur Rehman <muneebofficial@gmail.com>
2024-01-09 02:56:22 +0500

    getVideoById Done

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -18,24 +18,32 @@ const publishAVideo = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All Fiels Are Requried");
   }
 
-  const videoLocalPath = req.file.path;
+  const videoLocalPath = req.files.video[0].path;
+  const thumbnailLocalPath = req.files?.thumbnail[0].path
 
   if (!videoLocalPath) {
     throw new ApiError(404, "unable to get video local path");
   }
 
-  const publishedVideo = await uploadToCloudinary(videoLocalPath);
-
-  if (!publishedVideo) {
-    throw new ApiError(400, "Unable to Upload a Video File");
+  if(!thumbnailLocalPath){
+    throw new ApiError(404, "unable to get Thumbnail local path");
   }
 
-  const videoUrl = publishedVideo.url;
-  const thumbnail = videoUrl.replace(path.extname(videoUrl), ".jpg");
+  const publishedVideo = await uploadToCloudinary(videoLocalPath);
+  const videoThumbnail = await uploadToCloudinary(thumbnailLocalPath)
+
+  if (!publishedVideo) {
+    throw new ApiError(400, "Unable to Upload a Video File to Cloudinary");
+  }
+
+  if(!videoThumbnail){
+    throw new ApiError(400, " Unable to upload video Thumbnail to Cloudinary")
+  }
+
 
   const video = await Video.create({
-    videoFile: videoUrl,
-    thumbnail: thumbnail,
+    videoFile:  publishedVideo.url,
+    thumbnail : videoThumbnail.url,
     title,
     description,
     duration: Math.floor(publishedVideo.duration),
@@ -52,4 +60,51 @@ const publishAVideo = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, video, "Video Uploaded Successfully"));
 });
 
-export { getAllVideos, publishAVideo };
+
+const getVideoById = asyncHandler(async (req, res) => {
+  const { videoId } = req.params
+
+  const video = await Video.aggregate([
+    {
+      $match : {
+        _id : new mongoose.Types.ObjectId(videoId)
+      }
+    },
+    {
+      $lookup : {
+        from : "users",
+        localField : "owner",
+        foreignField : "_id",
+        as : "owner",
+        pipeline : [
+          {
+            $project : {
+              email : 1,
+              fullName : 1,
+              avatar: 1,
+              username : 1,
+            }
+          },
+        
+        ]
+      }
+    },
+    {
+      $addFields : {
+        owner : {
+          $first : "$owner"
+        }
+      }
+    }
+
+  ])
+
+  if(!video){
+    throw new ApiError(404, "cant find Video")
+  }
+
+  return res.status(200).json(new ApiResponse(200, video, "Video Fetched Successfully"))
+
+})
+
+export { getAllVideos, publishAVideo, getVideoById };
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,13 +1,21 @@
 import { Router } from "express";
-import {upload } from '../middleware/multer.middleware.js'
+import { upload } from "../middleware/multer.middleware.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
-import { publishAVideo } from "../controllers/video.controller.js";
+import { getVideoById, publishAVideo } from "../controllers/video.controller.js";
 
+const videoRouter = Router();
 
-const videoRouter = Router()
+videoRouter.route("/upload").post(verifyJwt, upload.fields([
+    {
+        name: "video",
+        maxCount: 1
+    },
+    {
+        name : "thumbnail",
+        maxCount : 1
+    },
+]), publishAVideo);
 
+videoRouter.route("/id/:videoId").get(getVideoById)
 
-videoRouter.route('/upload').post(verifyJwt, upload.single("video"), publishAVideo)
-
-
-export default videoRouter
+export default videoRouter;

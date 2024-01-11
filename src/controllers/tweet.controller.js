41467e5e1754ee1a72db7e908fe8aa0dc21f5702
Muneeb ur Rehman <muneebofficial@gmail.com>
2024-01-12 02:38:50 +0500

    Tweet controller Done

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Tweet } from "../models/tweet.model.js";
 import { ApiError } from "../utils/ApiError.js";
@@ -30,6 +30,68 @@ const createTweet = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, tweet, "Tweet is created"));
 });
 
+const getUserTweets = asyncHandler(async (req, res) => {
+  // TODO: get user tweets
+  const { userId } = req.params;
 
+  const tweets = await Tweet.find({
+    owner: new mongoose.Types.ObjectId(userId),
+  });
 
-export { createTweet }
+  if (!tweets) {
+    throw new ApiError(404, "Tweets not found");
+  }
+
+  return res.status(200).json(new ApiResponse(200, tweets, "Tweets Fetched"));
+});
+
+const updateTweet = asyncHandler(async (req, res) => {
+  //TODO: update tweet
+  const { content } = req.body;
+  const userId = req.user?._id;
+  const { tweetId } = req.params;
+
+  if (!userId) {
+    throw new ApiError(400, "Login First");
+  }
+
+  const tweet = await Tweet.findByIdAndUpdate(
+    tweetId,
+    {
+      content,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!tweet) {
+    throw new ApiError(400, "Unable To Update the tweet");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Tweet Updated Successfully"));
+});
+
+const deleteTweet = asyncHandler(async (req, res) => {
+  //TODO: delete tweet
+  const userId = req.user?._id;
+  const { tweetId } = req.params;
+
+  if (!userId) {
+    throw new ApiError(400, "Login First");
+  }
+
+  const tweet = await Tweet.findByIdAndDelete(tweetId);
+
+  if (!tweet) {
+    throw new ApiError(400, "There is not Tweet Exists");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Tweet Deleted Successfully"));
+});
+
+export { createTweet, getUserTweets, updateTweet, deleteTweet };
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { verifyJwt } from "../middleware/auth.middleware.js";
-import { createTweet } from "../controllers/tweet.controller.js";
+import { createTweet, deleteTweet, getUserTweets, updateTweet } from "../controllers/tweet.controller.js";
 
 const tweetRouter =  Router();
 
 tweetRouter.use(verifyJwt)
 
 tweetRouter.route('/').post(createTweet)
+tweetRouter.route('/user/:userId').get(getUserTweets)
+tweetRouter.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
 
 
 export default tweetRouter

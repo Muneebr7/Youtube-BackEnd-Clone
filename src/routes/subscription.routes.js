ca95efa2c52025f20f396a63e1ce651c104dc82a
Muneeb ur Rehman <muneebofficial@gmail.com>
2024-01-13 23:33:59 +0500

    Working  on Subscription Schema

---
+++ src/routes/subscription.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import  verifyJwt from '../middleware/auth.middleware.js'
+import { getSubscribedChannels, getUserChannelSubscribers, toggleSubscription } from "../controllers/subscription.controller";
+
+
+const subscriptionRouter =  Router();
+
+subscriptionRouter.use(verifyJwt)
+
+
+subscriptionRouter.route('/channel/:channelId').get(getSubscribedChannels).post(toggleSubscription)
+subscriptionRouter.route('/u/:subscriberId').get(getUserChannelSubscribers)

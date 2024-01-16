bb8bcefa81bb7c7e5ec7bdef00eaafc531c9a1e9
Muneeb ur Rehman <muneebofficial@gmail.com>
2024-01-16 23:21:42 +0500

    working comment routes

--- src/app.js
+++ src/app.js
@@ -19,9 +19,11 @@ import router from './routes/user.routes.js'
 import videoRouter from './routes/video.routes.js'
 import tweetRouter from './routes/tweet.routes.js'
 import playlistRouter from './routes/playlist.routes.js'
+import commentRouter from './routes/comment.routes.js'
 
 app.use('/api/v1/users' , router)
 app.use('/api/v1/videos' , videoRouter)
 app.use('/api/v1/tweets' , tweetRouter)
 app.use('/api/v1/playlist' ,  playlistRouter)
+app.use('/api/v1/comments' , commentRouter)
 export {app}

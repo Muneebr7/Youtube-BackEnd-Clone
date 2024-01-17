f626998a3d3dadd5d4a4187a69dbc9e183720a0a
Muneeb ur Rehman <muneebofficial@gmail.com>
2024-01-17 23:20:47 +0500

    dashboard controllers done

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -9,7 +9,47 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
     const channelId = req.user?._id
-    
+
+    if(!isValidObjectId(channelId) || !channelId){
+        throw new ApiError(400, "unAuthorized Request. please Login")
+    }
+
+    // total video Views
+    const totalVideoViews = await Video.aggregate([
+        {
+            $match : {
+                _id : new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $group : {
+                _id : null,
+                totalViews : {$sum : "$views"}
+            }
+        }
+    ])
+
+    if(!totalVideoViews){
+        throw new ApiError(400, "Unable to get total Video Views")
+    }
+
+    // get Total Subscriber
+    const totalSubscribers = await Subscription.countDocuments({
+        channel : channelId
+    })
+
+    // get Total Videos
+    const totalVideos = await Video.countDocuments({
+        owner : channelId
+    })
+
+    const data = {
+        totalSubscribers,
+        totalVideoViews,
+        totalVideos
+    }
+
+    return res.status(200).json(new ApiResponse(200, data , "Fetched Channel Stats"))
     
 
 
